e3ec4bc1ea3ac749ed1b7ff8a071e6b05d9e70b2
Thatchayuth.t <Thatchayuth.t@fsa.co.th>
2025-09-10 20:29:46 +0700

    ..

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -21,13 +21,13 @@ export const appConfig: ApplicationConfig = {
       SocialLoginModule.initialize({
         autoLogin: false,
         providers: [
-          {
-            id: GoogleLoginProvider.PROVIDER_ID,
-            provider: new GoogleLoginProvider('304753457158-e6gg6gn4c5kua773fihgm4ad7a8v256v.apps.googleusercontent.com'),
-          },
+          // {
+          //   id: GoogleLoginProvider.PROVIDER_ID,
+          //   provider: new GoogleLoginProvider('304753457158-e6gg6gn4c5kua773fihgm4ad7a8v256v.apps.googleusercontent.com'),
+          // },
           {
             id: FacebookLoginProvider.PROVIDER_ID,
-            provider: new FacebookLoginProvider('EAALLDba4OyQBPT3UKj3ZARZCYzw8S4Ara2SlV6CORl3YBBYuFjsMGCoZBHV8ZAT3JBzLDcaBoUfkqw0ZCt8H607YdSMBCYzUoMGZAG5ZANDor4ikGZBkPAYe2VShZBMtSUEeGxJdeDLz7MdGlHuja6vN3riI89DM1x94I3S4FBpNAOAMb2aUtm8NOHp7ikqwSx98WGAfaaJCy4bPhy4VVs2ZAouan0pbK8nvB6PeLMeEW2al2qHp5DWgcFQeYe6DxuknVlSxgTXHqT9T8s4QZDZD'),
+            provider: new FacebookLoginProvider('1869109890625536'),
           },
         ],
       })
